ba1dc0cf5ca05d422e346aa5ded653882d738d0d
Maximilian Tagher <feedback.tagher@gmail.com>
2014-05-31 09:18:13 -0700

    Move voting out to a separate module

--- Handler/ProjectDetails.hs
+++ Handler/ProjectDetails.hs
@@ -4,6 +4,8 @@ import Import
 import qualified Data.Text as T
 import qualified Data.Text.Read as TR
 
+import App.Voting
+
 postProjectDetailsR :: ProjectId -> Handler Html
 postProjectDetailsR projectId = do
     project <- runDB $ get404 projectId
@@ -23,22 +25,3 @@ postProjectDetailsR projectId = do
     --                     let votes = read votes :: Int
     --    Nothing -> 
     --error "Not yet implemented: putProjectDetailsR"
-
-readInt :: Text -> Maybe Int
-readInt text = case TR.decimal text of
-                Right (int,_) -> Just int
-                Left _ -> Nothing -- Probably should return Nothing here, and default to 3
-
--- Gets the remaining votes for the given HackDay
--- If not present in the session, sets it to the default value.
-getVotes :: HackDayId -> Handler Int
-getVotes hackDayId = do
-    maybeVotesText <- lookupSession $ remainingVotesKey hackDayId
-    case maybeVotesText >>= readInt of
-        Just votes -> return votes
-        Nothing -> do
-            setSession (remainingVotesKey hackDayId) defaultVotesText
-            return defaultVotes
-
-voteFor :: ProjectId -> Handler ()
-voteFor projectId = runDB $ updateWhere [ProjectId ==. projectId] [ProjectVotes +=. 1]
---
+++ app/Voting.hs
@@ -0,0 +1,23 @@
+module App.Voting where
+
+import Import
+import qualified Data.Text.Read as TR
+
+readInt :: Text -> Maybe Int
+readInt text = case TR.decimal text of
+                Right (int,_) -> Just int
+                Left _ -> Nothing -- Probably should return Nothing here, and default to 3
+
+-- Gets the remaining votes for the given HackDay
+-- If not present in the session, sets it to the default value.
+getVotes :: HackDayId -> Handler Int
+getVotes hackDayId = do
+    maybeVotesText <- lookupSession $ remainingVotesKey hackDayId
+    case maybeVotesText >>= readInt of
+        Just votes -> return votes
+        Nothing -> do
+            setSession (remainingVotesKey hackDayId) defaultVotesText
+            return defaultVotes
+
+voteFor :: ProjectId -> Handler ()
+voteFor projectId = runDB $ updateWhere [ProjectId ==. projectId] [ProjectVotes +=. 1]
