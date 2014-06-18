088acf99143f88fb4cebc59df0a6fd8a511eb230
Maximilian Tagher <feedback.tagher@gmail.com>
2014-06-18 13:17:02 -0700

    Check for the hackday being closed before allowing a vote

--- Handler/ProjectDetails.hs
+++ Handler/ProjectDetails.hs
@@ -16,11 +16,13 @@ $(deriveJSON defaultOptions ''VoteResponse)
 postProjectDetailsR :: ProjectId -> Handler Value
 postProjectDetailsR projectId = do
     project <- runDB $ get404 projectId
+    hackday <- runDB $ get404 $ projectHackday project
     remainingVotes <- getVotes $ projectHackday project
-    if remainingVotes > 0
-        then do
-            setSession (remainingVotesKey $ projectHackday project) (T.pack $ show $ remainingVotes - 1)
-            voteFor projectId
-            returnJson $ VoteResponse {remainingVotes = remainingVotes - 1, message = Nothing }
-        else do
-            returnJson $ VoteResponse {remainingVotes = remainingVotes, message = Just "Out of votes!" }
+    if remainingVotes == 0
+        then returnJson $ VoteResponse {remainingVotes = remainingVotes, message = Just "Out of votes!" }
+        else if hackDayVotingClosed hackday
+            then returnJson $ VoteResponse {remainingVotes = remainingVotes, message = Just "Voting closed" }
+            else do
+                setSession (remainingVotesKey $ projectHackday project) (T.pack $ show $ remainingVotes - 1)
+                voteFor projectId
+                returnJson $ VoteResponse {remainingVotes = remainingVotes - 1, message = Nothing }
