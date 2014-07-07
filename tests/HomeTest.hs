60221b846b567f34559ccf8077a3ebd956740f40
Maximilian Tagher <feedback.tagher@gmail.com>
2014-07-06 17:22:53 -0700

    Clean up tests

--- tests/HomeTest.hs
+++ tests/HomeTest.hs
@@ -4,7 +4,6 @@ module HomeTest
     ) where
 
 import TestImport
-import qualified Data.List as L
 import Data.Time.Clock
 import Control.Monad.IO.Class (liftIO)
 import Database.Persist.Sql (rawExecute)
@@ -22,34 +21,34 @@ homeSpecs =
         yit "shows the current hackday" $ do
             _ <- runDB $ rawExecute "TRUNCATE TABLE hack_day, project;" []
             currentTime <- liftIO $ getCurrentTime
-            hackId <- runDB $ insert $ HackDay { hackDayTitle = "testTitle"
-                                               , hackDayCreated = currentTime
-                                               , hackDayVotingClosed = False }
+            _ <- runDB $ insert $ HackDay { hackDayTitle = "testTitle"
+                                          , hackDayCreated = currentTime
+                                          , hackDayVotingClosed = False }
             get HackDayR
             htmlAllContain ".currentHackday" "testTitle"
 
         yit "doesn't show the current hackday if closed" $ do
             _ <- runDB $ rawExecute "TRUNCATE TABLE hack_day, project;" []
             currentTime <- liftIO $ getCurrentTime
-            hackId <- runDB $ insert $ HackDay { hackDayTitle = "testTitle"
-                                               , hackDayCreated = currentTime
-                                               , hackDayVotingClosed = True }
+            _ <- runDB $ insert $ HackDay { hackDayTitle = "testTitle"
+                                          , hackDayCreated = currentTime
+                                          , hackDayVotingClosed = True }
             get HackDayR
             htmlNoneContain ".currentHackday" "testTitle"
 
         yit "shows the past hackdays" $ do
             _ <- runDB $ rawExecute "TRUNCATE TABLE hack_day, project;" []
             currentTime <- liftIO $ getCurrentTime
-            hackId <- runDB $ insert $ HackDay { hackDayTitle = "test1"
-                                               , hackDayCreated = currentTime
-                                               , hackDayVotingClosed = True }
+            _ <- runDB $ insert $ HackDay { hackDayTitle = "test1"
+                                          , hackDayCreated = currentTime
+                                          , hackDayVotingClosed = True }
 
-            hackId <- runDB $ insert $ HackDay { hackDayTitle = "test2"
-                                               , hackDayCreated = currentTime
-                                               , hackDayVotingClosed = True }
+            _ <- runDB $ insert $ HackDay { hackDayTitle = "test2"
+                                          , hackDayCreated = currentTime
+                                          , hackDayVotingClosed = True }
 
             get HackDayR
             htmlAnyContain "a" "test1"
-            htmlAnyContain "a" "test1"
+            htmlAnyContain "a" "test2"
 
 
